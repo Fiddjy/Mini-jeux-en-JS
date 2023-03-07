5afc9016b536d110e25db81275747a1d95a96e95
Alexandra LAGNEL <107137288+Fiddjy@users.noreply.github.com>
2023-03-07 13:42:29 +0000

    Proposer lettres + cacher lettres proposées + compteur d'erreur + fin de partie

--- pendu/pendu.js
+++ pendu/pendu.js
@@ -2,28 +2,36 @@
     V Générer un mot aléatoire
     V afficher le mot en masqué _ _ _ _ _
     V Pouvoir proposer des lettres
-    O Masquer les lettres proposées
-    O Afficher les lettres trouvées
-    O Gérer le nombre d'erreurs max
+    V Masquer les lettres proposées
+    V Afficher les lettres trouvées
+    V Gérer le nombre d'erreurs max
+    X Gérer la fin de partie
 */
 
 const buttonPlay = document.getElementById("beginGame");
 const allWords = ['ministre', 'congolais','constitution', 'corompre', 'petrole', 'dictateur', 'sapeur', 'prisonnier', 'chomage', 'economie'];
 const wordToFindDiv = document.getElementById("wordToFindDiv")
 const KeyBoardDiv = document.getElementById("KeyBoard")
+let wordToFind = '';
+let wordToFindArray;
+let cptErreur = 0
 
 buttonPlay.addEventListener("click", function(){
-    beginGame()
+    initGame()
 })
 
-function beginGame(){
+function initGame(){
     // Générer un mot au hasard
+    cptErreur = 0
     wordToFindDiv.innerHTML = ''
-    let wordToFind = generateWord()
-    let wordToFindArray = Array.from(wordToFind)
+    wordToFind  = generateWord()
+    wordToFindArray = Array.from(wordToFind)
 
     let table = document.createElement("table")
+
     let line = document.createElement("tr")
+    line.id="LineOfWord";
+
     wordToFindArray.forEach(letter => {
         //Créer un TD (case du tableau) par lettre
         let td = document.createElement("td")
@@ -45,6 +53,33 @@ function generateKeyBoard(){
         lettreDiv.innerHTML = letter
         lettreDiv.classList.add("letterKeyBoard")
         KeyBoardDiv.appendChild(lettreDiv)
+
+        lettreDiv.addEventListener("click", () => {
+            if(checkLetterInWord(letter)) { 
+                // TODO afficher la lettre dans le mot masqué
+                let lineWord = document.getElementById("LineOfWord") 
+                let allTdOfWord = lineWord.children
+
+                Array.from(allTdOfWord).forEach(td => {
+                    if(td.dataset.letter == letter){
+                        td.innerHTML = letter;
+                    }
+                })
+            }
+            else{
+                cptErreur++
+                document.getElementById("cptErreur").innerHTML = cptErreur
+                if(cptErreur >= 5) {
+                    document.getElementById("cptErreur").innerHTML = "Perdu, vous avez fait plus de 5 erreurs."
+                    let lineWord = document.getElementById("LineOfWord")
+                    let allTdOfWord = lineWord.children
+                    Array.from(allTdOfWord).forEach(td => {
+                        td.innerHTML = td.dataset.letter
+                    })
+                }
+            }
+            lettreDiv.style.visibility = "hidden"
+        })
     })
 }
 
@@ -70,4 +105,15 @@ function generateWord(){
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
+  }
+
+// retourne true si la lettre est présente dans le mot, retourn false si la lettre est absente du mot
+function checkLetterInWord(letter){
+    let findLetter = false
+    wordToFindArray.forEach(letterOfWord => {
+        if (letter == letterOfWord){
+                findLetter = true
+        }
+    })
+    return findLetter
   }
