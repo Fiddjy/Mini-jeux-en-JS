8f950be34403b43cf18da07ba6b894a36e43b9af
Alexandra LAGNEL <107137288+Fiddjy@users.noreply.github.com>
2023-03-07 15:11:10 +0000

    Création des fichiers du calcul mental

---
+++ CalculMental/calcul.js
@@ -0,0 +1,13 @@
+/* TODO LIST
+
+O Lancer un minuteur de x minute(s) => réutiliser celui du pendu
+O Générer un calcul (deux chiffres aléatoires, (+ - *) en aléatoire)
+O Laisser l'utilisateur faire des propositions
+
+V2 
+Paramétrer ma partie
+Le temps du compte à rebours
+Les opérateurs de la partie
+Meilleur temps en cookie
+
+*/
