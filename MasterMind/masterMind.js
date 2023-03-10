da16472092ee94e80b68745e6a7b15cf45ee5c11
Alexandra LAGNEL <107137288+Fiddjy@users.noreply.github.com>
2023-03-10 14:30:53 +0000

    ajout des inputs pour proposer les couleurs

--- MasterMind/masterMind.js
+++ MasterMind/masterMind.js
@@ -7,9 +7,31 @@ import { Utils } from "../lib/Utils/utils.js";
 
 const colors = ["red", "blue", "yellow", "pink"]
 let colorTabToFind = null
+const nbColorToFind = 4
 
-setAleaColorTab()
-console.log(colorTabToFind)
+document.getElementById("startButton").addEventListener("click", () => {
+    launchGame() 
+})
+
+function launchGame() {
+    setAleaColorTab()
+    document.getElementById("allSelect").innerHTML = ""
+    for(let index = 0; index < nbColorToFind; index++) {
+        generateSelect("allSelect")
+    }
+}
+
+function generateSelect(idCible){
+    let mySelect = document.createElement("select")
+    colors.forEach(color => {
+        let colorOption=document.createElement("option")
+        colorOption.innerHTML = color
+        colorOption.value = color
+        mySelect.appendChild(colorOption)
+        colorOption.style.backgroundColor = color
+    })
+    document.getElementById(idCible).appendChild(mySelect)
+}
 
 function setAleaColorTab(size = 4) {
     colorTabToFind = []
