782a525c25c8795b01b50d5e4a183ffe5afbae74
Alexandra LAGNEL <107137288+Fiddjy@users.noreply.github.com>
2023-03-10 14:09:21 +0000

    Générer la combinaison secrète de 4 couleurs

--- MasterMind/masterMind.js
+++ MasterMind/masterMind.js
@@ -1,10 +1,25 @@
 /* TODO LIST
-- Générer la combinaison secrète de 4 couleurs
+- Générer la combinaison secrète de 4 couleurs => DONE
 - Pouvoir proposer une combinaison
 - Gérer début et fin de partie (importer les confettis)
 */
 import { Utils } from "../lib/Utils/utils.js";
 
 const colors = ["red", "blue", "yellow", "pink"]
+let colorTabToFind = null
 
-console.log(colors[Utils.getRandomInt(4)])
+setAleaColorTab()
+console.log(colorTabToFind)
+
+function setAleaColorTab(size = 4) {
+    colorTabToFind = []
+    for(let index = 0; index < size; index++) {
+        colorTabToFind.push(getAleaColor())
+    }
+}
+
+function getAleaColor(){
+    let aleaIndex = Utils.getRandomInt(colors.length)
+    let aleaColor = colors[aleaIndex]
+    return aleaColor
+}
